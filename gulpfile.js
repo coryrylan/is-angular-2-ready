85139cb100eb4914bea01215d6c366707b8327d0
Cory Bateman <splintercode.cb@gmail.com>
2015-09-12 11:28:26 -0500

    added local dev serv task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@
 let gulp = require('gulp-help')(require('gulp'));
 let tsc = require('gulp-typescript');
 let tslint = require('gulp-tslint');
-
+let http = require('http');
+let connect = require('connect');
+let serveStatic = require('serve-static');
+let open = require('open');
+    
 const DOCS = {
     typescriptBuild:    'Build TypeScript and compile out ES5 JavaScript',
     typescriptLint:     'Lint TypeScript to check for style and syntax errors.',
-    watch:              'Start watching files for compilation and linting.'
+    watch:              'Start watching files for compilation and linting.',
+    runDev:             'Start dev server and watch tasks.'
 };
 
 const SOURCE = {
@@ -35,6 +40,16 @@ gulp.task('build.typescript', DOCS.typescriptBuild, [], () => {
 
 gulp.task('watch', DOCS.watch, () => {
     gulp.watch(SOURCE.typescript, ['build.typescript']).on('error', catchError);
+});
+
+gulp.task('run.dev', DOCS.runDev, ['watch', 'build.typescript'], function () {
+    let port = 9000, app;
+
+    app = connect().use(serveStatic(__dirname));  // serve everything that is static
+    
+    http.createServer(app).listen(port, function () {
+        open('http://localhost:' + port);
+    });
 });
 
 function catchError(err) {
